70e81663f06f6ebed9774ad04a334e50046e3ac2
Anderson <andeersonporteela@gmail.com>
2022-04-12 00:33:39 -0300

    Sanity fully linked to the application

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,9 +2,13 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: '15z9qx6x',
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: 'skXWmjGEL1sD7ddYCZpk51KxVAVhjplgp8VpuMcEBe7eOwXY7MWRyL82YsdVijwIOmxlUhCQ3qGk8EVI2RSxqVbndub5PNZ3nKsIrPmhqAg83xl5vT03XWlnKNtxu2HSruPqeHw3P0qLlYx3pC8YiF1kisxoxX6smjLXSSI8xzFW2nPeuHFi',
+    token: process.env.REACT_APP_SANITY_TOKEN,
 });
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,15 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
+import { urlFor, client } from '../../client';
 import './About.scss';
 
-const abouts = [
-  { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01 },
-  { title: 'Frontend Developer', description: 'I am a good web developer.', imgUrl: images.about02 },
-  { title: 'UI/UX', description: 'I am a good web developer.', imgUrl: images.about03 }
-]
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, []);
+  
+
+
   return (
     <>
       <h2 className="head-text">We know that <span>Good Development</span><br />means <span>Good Business</span></h2>
@@ -24,7 +31,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20}}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10}}>{about.description}</p>
           </motion.div>
