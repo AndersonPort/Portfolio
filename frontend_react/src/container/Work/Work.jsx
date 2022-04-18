e98428335ee755054d7ac23019d6828975519ea6
Anderson <andeersonporteela@gmail.com>
2022-04-17 23:37:07 -0300

    Working on work section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
+
+  const [activeFilter, setActiveFilter] = useState('All');
+
+  const handleWorkFilter = (item) => {
+
+  }
+
   return (
-    <div>Work</div>
+    <>
+      <h2 className="head-text">My creative <span>Portfolio</span>Section</h2>
+    
+      <div className="app__work-filter">
+        {[ 'UI/UX', 'Web App', 'Mobile App', 'React JS', 'All' ].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-tex ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
