bd4b416e9f709290b2ce827c31ee230de0c8effa
Anderson <andeersonporteela@gmail.com>
2022-04-18 23:50:48 -0300

    Work section done

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -24,7 +24,18 @@ const Work = () => {
   
 
   const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y:100, opacity: 0 }]);
 
+    setTimeout(() => {
+      setAnimateCard([{ y:0, opacity: 1 }]);
+
+      if(item ==='All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)))
+      }
+    }, 500);
   }
 
   return (
